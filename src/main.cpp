4e08ab073c7505d315a870aef5c1363217f52a81
Sopwit <emirkanbur89@gmail.com>
2026-05-13 00:02:35 +0300

    Force software GL by default

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+#include <QByteArray>
+#include <QCoreApplication>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
+    if (!qEnvironmentVariableIsSet("LIBGL_ALWAYS_SOFTWARE")) {
+        qputenv("LIBGL_ALWAYS_SOFTWARE", QByteArrayLiteral("1"));
+    }
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
